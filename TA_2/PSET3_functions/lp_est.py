3fa22446de48abf32fc45df5d9138c8c17a127ae
jaketg97 <jaketg97@gmail.com>
2024-10-29 10:36:52 -0500

    updating my PSET code in case any eyes are on it

--- TA_2/PSET3_functions/lp_est.py
+++ TA_2/PSET3_functions/lp_est.py
@@ -4,6 +4,28 @@ import jax.numpy as jnp
 import jaxopt as jaxopt
 from PSET3_functions.misc import poly_2v, jnp_reg_predict, jnp_reg
 
+def LP_firststage(dat): 
+
+    LP_data = dat.copy()
+    #####
+    # LP First Stage (from Stata Journal)
+    #####
+
+    LP_data, phi_vars = poly_2v("k", "m", LP_data)
+    constant = jnp.ones((LP_data.shape[0], 1))
+    phi_var_mat = jnp.array(LP_data[phi_vars].to_numpy())
+    l = LP_data["l"].copy().to_numpy().reshape(-1, 1)
+    X = jnp.hstack((constant, phi_var_mat, l))
+    y = LP_data["y_gross"].copy()
+    lp_first_stage = sm.OLS(LP_data["y_gross"], sm.add_constant(LP_data[phi_vars + ["l"]])).fit()
+    beta_hat_1 = lp_first_stage.params['l']
+    # LP_data["y_gross_hat"] = jnp_reg_predict(y, X)
+    beta_hat_stage1 = jnp_reg(y, X)
+    beta_hat_2 = beta_hat_stage1[len(beta_hat_stage1)].item()
+    # LP_data["y_gross_hat"] = lp_first_stage.predict(sm.add_constant(LP_data[phi_vars + ["l"]]))
+    # LP_data["phi_prediction"] = LP_data["y_gross_hat"] - beta_hat_l * LP_data["l"]
+    return beta_hat_1, beta_hat_2
+
 
 def LP_estimation(dat):
     
@@ -15,14 +37,15 @@ def LP_estimation(dat):
     LP_data, phi_vars = poly_2v("k", "m", LP_data)
     constant = jnp.ones((LP_data.shape[0], 1))
     phi_var_mat = jnp.array(LP_data[phi_vars].to_numpy())
-    l = LP_data["l"]
+    l = LP_data["l"].copy().to_numpy().reshape(-1, 1)
     X = jnp.hstack((constant, phi_var_mat, l))
-    y = LP_data["y"].copy()
+    y = LP_data["y_gross"].copy()
     # lp_first_stage = sm.OLS(LP_data["y_gross"], sm.add_constant(LP_data[phi_vars + ["l"]])).fit()
-    # lp_first_stage.params["l"]
+    # beta_hat_l = lp_first_stage.params["l"]
+    # LP_data["y_gross_hat"] = lp_first_stage.predict(sm.add_constant(LP_data[phi_vars + ["l"]]))
     LP_data["y_gross_hat"] = jnp_reg_predict(y, X)
     beta_hat_stage1 = jnp_reg(y, X)
-    beta_hat_l = beta_hat_stage1[len(beta_hat_stage1)]
+    beta_hat_l = beta_hat_stage1[len(beta_hat_stage1)].item()
     LP_data["phi_prediction"] = LP_data["y_gross_hat"] - beta_hat_l * LP_data["l"]
 
     #####
@@ -57,7 +80,7 @@ def LP_estimation(dat):
         g3 = jnp.power(omega_lag, 3).reshape(-1, 1)
         g_func = jnp.hstack((jnp.ones((g.shape[0],1)),g, g2, g3))
         omega_hat = jnp_reg_predict(omega, g_func)
-        ksi_epsilon = dat[:, column_indices["y_gross"]] - lp_first_stage.params["l"] * dat[:, column_indices["l"]] - beta_k * dat[:, column_indices["k"]] - beta_m * dat[:, column_indices["m"]] - omega_hat
+        ksi_epsilon = dat[:, column_indices["y_gross"]] - beta_hat_l * dat[:, column_indices["l"]] - beta_k * dat[:, column_indices["k"]] - beta_m * dat[:, column_indices["m"]] - omega_hat
         instruments = ["k", "m_lag", "l_lag", "m_lag_2", "k_lag"]
         moments = jnp.array([])
         for z in instruments:
@@ -69,4 +92,4 @@ def LP_estimation(dat):
     solver = jaxopt.BFGS(fun = LP_GMM_val_grad)
     res = solver.run(np.array([1.0,1.0]), data = LP_data.to_numpy())
     beta_k, beta_m = res.params.tolist()
-    return(beta_k, beta_m, lp_first_stage.params["l"])
+    return(beta_k, beta_m, beta_hat_l)
